7814b6abdd7abbffd689ffc29e233f715ec315e8
Konstantin <KonstantKar@yandex.ru>
2023-09-01 16:44:11 +0300

    Создание всех папок и базового CSS

---
+++ src/components/brand/Brand.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import "./Brand.css";
+const Brand = () => {
+  return <div></div>;
+};
+
+export default Brand;
---
+++ src/containers/whatGPT/WhatGPT.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import "./WhatGPT";
+const WhatGPT = () => {
+  return <div>WhatGPT</div>;
+};
+
+export default WhatGPT;
