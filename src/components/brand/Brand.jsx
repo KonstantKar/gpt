71ea94b1de28d79709454791e2e0db5992b74f97
Konstantin <KonstantKar@yandex.ru>
2023-09-02 17:28:42 +0300

    Оформлен Brand ( адаптив )

--- src/components/brand/Brand.jsx
+++ src/components/brand/Brand.jsx
@@ -1,7 +1,23 @@
 import React from "react";
 import "./Brand.css";
+import google from "../../assets/google.png";
+import slack from "../../assets/slack.png";
+import atlassian from "../../assets/atlassian.png";
+import dropbox from "../../assets/dropbox.png";
+import shopify from "../../assets/shopify.png";
+
 const Brand = () => {
-  return <div></div>;
+  return (
+    <div className="gpt3__brand section__padding">
+      <div className="gpt3__brand-images">
+        <img src={google} alt="google" />
+        <img src={slack} alt="slack" />
+        <img src={atlassian} alt="atlassian" />
+        <img src={dropbox} alt="dropbox" />
+        <img src={shopify} alt="shopify" />
+      </div>
+    </div>
+  );
 };
 
 export default Brand;
