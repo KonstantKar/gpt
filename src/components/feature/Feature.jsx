b42a8e7462024aad85ac828592371109fe094640
Konstantin <KonstantKar@yandex.ru>
2023-09-02 21:51:16 +0300

    Оформлен WhatGPT ( адаптивен ) + компонента Feature

--- src/components/feature/Feature.jsx
+++ src/components/feature/Feature.jsx
@@ -1,7 +1,15 @@
 import React from "react";
 import "./Feature.css";
-const Feature = () => {
-  return <div></div>;
+const Feature = ({ title, text }) => {
+  return (
+    <div className="gpt3__feature-container__feature">
+      <div className="gpt3__feature-container__feature-title">
+        <div></div>
+        <h1>{title}</h1>
+      </div>
+      <div className="gpt3__feature-container__feature-text">{text}</div>
+    </div>
+  );
 };
 
 export default Feature;
