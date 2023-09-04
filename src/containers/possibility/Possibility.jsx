6ef316cee58704b23e8e531dc4c5e799a7e2fb4d
Konstantin <KonstantKar@yandex.ru>
2023-09-04 03:08:59 +0300

    Оформлен Possibility ( адаптив )

--- src/containers/possibility/Possibility.jsx
+++ src/containers/possibility/Possibility.jsx
@@ -1,7 +1,31 @@
 import React from "react";
 import "./Possibility.css";
+import possibility from "../../assets/possibility.png";
 const Possibility = () => {
-  return <div>Possibility</div>;
+  return (
+    <div className="gpt3__possibility possibility__padding">
+      <div className="gpt3__possibility-img">
+        <img src={possibility} alt="image"></img>
+      </div>
+      <div className="gpt3__possibility-info">
+        <div className="gpt3__possibility-info__early">
+          Request Early Access to Get Started
+        </div>
+        <h1 className="gradient__text">
+          The possibilities are beyond your imagination
+        </h1>
+        <p>
+          Yet bed any for travelling assistance indulgence unpleasing. Not
+          thoughts all exercise blessing. Indulgence way everything joy
+          alteration boisterous the attachment. Party we years to order allow
+          asked of.
+        </p>
+        <div className="gpt3__possibility-info__request">
+          Request Early Access to Get Started
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Possibility;
